ef2d08129c8c0e475c6035bcebb76c620ba63bb3
PelajarPalingArif <arifraihan062003@gmail.com>
2025-01-11 15:37:19 +0800

    Added colored logging

--- src/main/java/com/astar/Main.java
+++ src/main/java/com/astar/Main.java
@@ -5,15 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends Application {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
     @Override
     public void start(Stage stage) {
         try {
+            logger.info("------ STARTING APPLICATION ------");
             Parent root = FXMLLoader.load(getClass().getResource("/com/astar/scene.fxml"));
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("/com/astar/style.css").toExternalForm());
@@ -21,8 +25,11 @@ public class Main extends Application {
             stage.setTitle("Test Application");
             stage.setScene(scene);
             stage.show();
+            logger.info("------ START SUCCESSFUL ------");
 
         } catch (IOException e) {
+            logger.debug("Fail");
+            logger.error(e.getMessage());
             throw new RuntimeException(e);
         }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module main {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.slf4j;
 
     opens com.astar to javafx.fxml;
     opens com.astar.controller to javafx.fxml;
